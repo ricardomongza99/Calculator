6bc7ac5d5db4e3279837a8d2c92353cf6c0f6fb6
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 17:21:36 -0500

    Add properties to ButtonStyle

--- Calculator/Styles/CalculatorButtonStyle.swift
+++ Calculator/Styles/CalculatorButtonStyle.swift
@@ -7,14 +7,36 @@
 
 import SwiftUI
 
-struct CalculatorButtonStyle: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CalculatorButtonStyle: ButtonStyle {
+    
+    var size: CGFloat
+    var backgroundColor: Color
+    var foregroundColor: Color
+    
+    func makeBody(configuration: Configuration) -> some View {
+            configuration.label
+                .font(.system(size: 32, weight: .medium))
+                .frame(width: size, height: size)
+                .background(backgroundColor)
+                .foregroundColor(foregroundColor)
+                .overlay {
+                    if configuration.isPressed {
+                        Color(white: 1.0, opacity: 0.2)
+                    }
+                }
+                .clipShape(Capsule())
     }
 }
 
 struct CalculatorButtonStyle_Previews: PreviewProvider {
+    static let buttonType: ButtonType = .digit(.four)
+    
     static var previews: some View {
-        CalculatorButtonStyle()
+        Button(buttonType.description) { }
+            .buttonStyle(CalculatorButtonStyle(
+                size: 80,
+                backgroundColor: buttonType.backgroundColor,
+                foregroundColor: buttonType.foregroundColor)
+            )
     }
 }
