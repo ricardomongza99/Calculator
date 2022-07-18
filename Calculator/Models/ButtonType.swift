d3a4a0f57f163b19f368a73cbdd89cf4fed67afb
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 15:05:47 -0500

    Add foregroundColor and backgroundColor to `ButtonType`

--- Calculator/Models/ButtonType.swift
+++ Calculator/Models/ButtonType.swift
@@ -6,14 +6,56 @@
 //
 
 import Foundation
+import SwiftUI
 
-enum ButtonType {
+enum ButtonType: Hashable, CustomStringConvertible {
     case digit(_ digit: Digit)
-    case arithmeticOperation(_ operation: ArithmeticOperation)
+    case operation(_ operation: ArithmeticOperation)
     case negative
     case percent
     case decimal
     case equals
     case allClear
     case clear
+    
+    var description: String {
+        switch self {
+        case .digit(let digit):
+            return digit.description
+        case .operation(let operation):
+            return operation.description
+        case .negative:
+            return "±"
+        case .percent:
+            return "%"
+        case .decimal:
+            return "."
+        case .equals:
+            return "="
+        case .allClear:
+            return "AC"
+        case .clear:
+            return "C"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .allClear, .clear, .negative, .percent:
+            return Color(.lightGray)
+        case .operation, .equals:
+            return .orange
+        case .digit, .decimal:
+            return .secondary
+        }
+    }
+    
+    var foregroundColor: Color {
+        switch self {
+        case .allClear, .clear, .negative, .percent:
+            return .black
+        default:
+            return .white
+        }
+    }
 }
