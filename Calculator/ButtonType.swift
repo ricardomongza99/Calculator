017da005bdcd2f995ec83ba49a108ee237e94fe4
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 13:35:20 -0500

    Create `ButtonType`

---
+++ Calculator/ButtonType.swift
@@ -0,0 +1,31 @@
+//
+//  ButtonType.swift
+//  Calculator
+//
+//  Created by Ricardo Montemayor on 18/07/22.
+//
+
+import Foundation
+
+enum ButtonType {
+    case zero
+    case one
+    case two
+    case three
+    case four
+    case five
+    case six
+    case seven
+    case eight
+    case nine
+    case addition
+    case subtraction
+    case multiplication
+    case division
+    case negative
+    case percent
+    case decimal
+    case equals
+    case allClear
+    case clear
+}
