f82095a01ec9a9e99636330822c52daad66d0ccf
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 16:57:22 -0500

    Create `CalculatorButtonStyle`

---
+++ Calculator/Styles/CalculatorButtonStyle.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorButtonStyle.swift
+//  Calculator
+//
+//  Created by Ricardo Montemayor on 18/07/22.
+//
+
+import SwiftUI
+
+struct CalculatorButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorButtonStyle()
+    }
+}
