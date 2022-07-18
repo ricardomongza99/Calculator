78ca5d90b553bc79434c26f5a385e3a2dc2770bc
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 12:08:42 -0500

    Create `CalculatorView`

---
+++ Calculator/CalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorView.swift
+//  Calculator
+//
+//  Created by Ricardo Montemayor on 18/07/22.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalculatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculatorView()
+    }
+}
