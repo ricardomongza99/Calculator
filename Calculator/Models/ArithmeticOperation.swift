179b0ad14b29ced2132fe4c91ba44efca13b0496
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 14:24:56 -0500

    Encapsulate ButtonType cases

--- Calculator/Models/ArithmeticOperation.swift
+++ Calculator/Models/ArithmeticOperation.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+enum ArithmeticOperation: CaseIterable, CustomStringConvertible {
+    case addition, subtraction, multiplication, division
+    
+    var description: String {
+        switch self {
+        case .addition:
+            return "+"
+        case .subtraction:
+            return "−"
+        case .multiplication:
+            return "×"
+        case .division:
+            return "÷"
+        }
+    }
+}
--- Calculator/Models/ButtonType.swift
+++ Calculator/Models/ButtonType.swift
@@ -8,20 +8,8 @@
 import Foundation
 
 enum ButtonType {
-    case zero
-    case one
-    case two
-    case three
-    case four
-    case five
-    case six
-    case seven
-    case eight
-    case nine
-    case addition
-    case subtraction
-    case multiplication
-    case division
+    case digit(_ digit: Digit)
+    case arithmeticOperation(_ operation: ArithmeticOperation)
     case negative
     case percent
     case decimal
