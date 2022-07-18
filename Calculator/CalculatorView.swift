d3efc8cdacf6c5cd7ae1dfe2c9dcdae4d5a11e8b
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 13:23:47 -0500

    Add `displayText` modifiers

--- Calculator/CalculatorView.swift
+++ Calculator/CalculatorView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct CalculatorView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("0")
+                .padding()
+                .foregroundColor(.white)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .font(.system(size: 88, weight: .light))
+                .lineLimit(1)
+                .minimumScaleFactor(0.2)
+            // TODO: ButtonPad
+        }
+        .background(Color.black)
     }
 }
 
