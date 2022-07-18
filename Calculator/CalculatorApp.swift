7127f08592777758391666cae10839ab865f8117
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-18 12:07:30 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ricardo Montemayor on 18/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
