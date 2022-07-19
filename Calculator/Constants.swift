0c48f77580d69a70a43779f356b78d2cdbb05c39
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-19 11:37:02 -0500

    Create `Constants`

---
+++ Calculator/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Calculator
+//
+//  Created by Ricardo Montemayor on 19/07/22.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Constants {
+    static let padding: CGFloat = 12.0
+}
