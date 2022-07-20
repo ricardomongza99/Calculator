f28cbba38f564708d5c9e59f90b31a3925e9ea8e
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-20 12:02:05 -0500

    Add Calculator boilerplate

--- Calculator/Models/Calculator.swift
+++ Calculator/Models/Calculator.swift
@@ -6,3 +6,50 @@
 //
 
 import Foundation
+
+struct Calculator {
+    
+    // MARK: - COMPUTED PROPERTIES
+        
+    var number: Decimal? {
+        return 0
+    }
+    
+    var displayText: String {
+        return "0"
+    }
+    
+    // MARK: - OPERATIONS
+    
+    mutating func setDigit(_ digit: Digit) {
+
+    }
+    
+    mutating func setOperation(_ operation: ArithmeticOperation) {
+        
+    }
+    
+    mutating func toggleSign() {
+        
+    }
+    
+    mutating func setPercent() {
+        
+    }
+    
+    mutating func setDecimal() {
+        
+    }
+    
+    mutating func setEquals() {
+        
+    }
+    
+    mutating func allClear() {
+        
+    }
+    
+    mutating func clear() {
+        
+    }
+}
