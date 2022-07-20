86cea8f466359dae3a75d8e5f29af0d35e409321
Ricardo Montemayor <ricardomongza@gmail.com>
2022-07-20 11:33:28 -0500

    Add ViewModel

--- Calculator/CalculatorApp.swift
+++ Calculator/CalculatorApp.swift
@@ -12,6 +12,7 @@ struct CalculatorApp: App {
     var body: some Scene {
         WindowGroup {
             CalculatorView()
+                .environmentObject(CalculatorView.ViewModel())
         }
     }
 }
--- Calculator/Views/CalculatorViewModel.swift
+++ Calculator/Views/CalculatorViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import Combine
+
+extension CalculatorView {
+    final class ViewModel: ObservableObject {
+        
+        // MARK: - PROPERTIES
+        
+        var buttonTypes: [[ButtonType]] {
+            [[.allClear, .negative, .percent, .operation(.division)],
+             [.digit(.seven), .digit(.eight), .digit(.nine), .operation(.multiplication)],
+             [.digit(.four), .digit(.five), .digit(.six), .operation(.subtraction)],
+             [.digit(.one), .digit(.two), .digit(.three), .operation(.addition)],
+             [.digit(.zero), .decimal, .equals]]
+        }        
+    }
+}
